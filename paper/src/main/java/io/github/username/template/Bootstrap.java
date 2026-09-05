cc66ad4bf3c3bc85c8f1645a2e97db735ad2df41
Tim <tim@timakai.me>
2026-09-05 22:07:33 +0200

    paper: Remove eager command creation in bootstrap
    Signed-off-by: Tim <tim@timakai.me>

--- paper/src/main/java/io/github/username/template/Bootstrap.java
+++ paper/src/main/java/io/github/username/template/Bootstrap.java
@@ -2,10 +2,7 @@ package io.github.username.template;
 
 import io.github.username.template.command.*;
 import io.papermc.paper.plugin.bootstrap.*;
-import io.papermc.paper.plugin.lifecycle.event.*;
-import io.papermc.paper.plugin.lifecycle.event.handler.configuration.*;
 import io.papermc.paper.plugin.lifecycle.event.types.*;
-import java.util.concurrent.*;
 import org.bukkit.plugin.java.*;
 import org.jetbrains.annotations.*;
 
@@ -15,12 +12,10 @@ public final class Bootstrap implements PluginBootstrap {
     @Override
     public void bootstrap(@NotNull BootstrapContext context) {
         var lifecycleManager = context.getLifecycleManager();
-        lifecycleManager.registerEventHandler(createCommandsEventHandler());
-    }
 
-    private static LifecycleEventHandlerConfiguration<@NotNull LifecycleEventOwner> createCommandsEventHandler() {
-        var command = CompletableFuture.supplyAsync(Command::create);
-        return LifecycleEvents.COMMANDS.newHandler(event -> event.registrar().register(command.join()));
+        lifecycleManager.registerEventHandler(LifecycleEvents.COMMANDS, commands -> {
+            commands.registrar().register(Command.create());
+        });
     }
 
     @Override
